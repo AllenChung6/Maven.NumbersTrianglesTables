4f9563b34230c956e07fabaf95a86aab3e084409
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-02-28 18:45:10 -0500

    Finished all methods except SquareNumbers and ExponentialNumbers.

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,31 +3,44 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i=start; i < stop; i++) {
+            if(i % 2 ==0)
+                 result += i;
+        }
+        return result;
     }
-
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String result = "";
+        for (int i=start; i < stop; i++) {
+            if(i % 2 ==1)
+                result += i;
+        }
+        return result;
     }
-
 
     public static String getSquareNumbers(int start, int stop, int step) {
         return null;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        return NumberUtilities.getRange(0,stop,1);
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        return NumberUtilities.getRange(start,stop,1);
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String result = "";
+        for (int i=start; i< stop; i= i + step) {
+            result += i;
+        }
+        return result;
     }
+
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
