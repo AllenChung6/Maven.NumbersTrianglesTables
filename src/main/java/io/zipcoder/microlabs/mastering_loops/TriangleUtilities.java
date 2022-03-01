0090ee09f7af3820245696632f9feee80d347dba
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-01 00:24:57 -0500

    Finished getRows method in tri util.

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,38 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
+        /*String pyramid = "";
+        String star = "*";
+        for (int i=0; i < numberOfRows; i++)
+            for (int j=0; j< numberOfRows; i++)
+                pyramid += i;
+       return pyramid; */
         return null;
+
     }
 
-    public static String getRow(int numberOfStars) {
-        return null;
+    public static String getRow(int width) {
+        String rows = " ";
+
+        for (int i = 0; i < width; i++) {
+            rows += "*";
+        }
+        return rows.replaceAll("\\s", "");
     }
 
     public static String getSmallTriangle() {
-        return null;
+        String result = "";
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i;  j++) {
+                result += "*";
+            }
+        }
+        return result;
     }
 
     public static String getLargeTriangle() {
         return null;
     }
+
 }
