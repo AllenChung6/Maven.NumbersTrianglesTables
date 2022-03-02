a14ee78827bf046c13ea7bc9c1b7abe45b4e45e1
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-02 00:09:11 -0500

    Finished all methids in lab. Tests passed.

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -1,15 +1,25 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
-    public static String getSmallMultiplicationTable() {
-        return null;
+    public static String getSmallMultiplicationTable(int tableSize) {
+        return TableUtilities.getMultiplicationTable(tableSize);
     }
 
-    public static String getLargeMultiplicationTable() {
-        return null;
+    public static String getLargeMultiplicationTable(int tableSize) {
+        return TableUtilities.getMultiplicationTable(tableSize);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String print = "";
+
+        for (int i = 1; i <= tableSize; i++) {
+            for (int j = 1; j <= tableSize; j++) {
+                /*print += i * j; */
+                print += String.format("%3d |", i*j);
+                //System.out.println(print);
+            }
+            print += "\n";
+        }
+        return print;
     }
 }
--- src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,21 @@ package io.zipcoder.microlabs.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        /*String pyramid = "";
-        String star = "*";
-        for (int i=0; i < numberOfRows; i++)
-            for (int j=0; j< numberOfRows; i++)
-                pyramid += i;
-       return pyramid; */
-        return null;
-
+        String triangle = "";
+        String row = "";
+        for (int i = 1; i < numberOfRows; i++) {
+//            for (int j = 0; j < i; j++) {
+//                row += "*";
+//                //System.out.println(triangle);
+//            }
+//            triangle += row + "\n";
+            triangle += TriangleUtilities.getRow(i) + "\n";
+        }
+        return triangle;
     }
 
     public static String getRow(int width) {
-        String rows = " ";
+        String rows = "";
 
         for (int i = 0; i < width; i++) {
             rows += "*";
@@ -22,19 +25,14 @@ public class TriangleUtilities {
         return rows.replaceAll("\\s", "");
     }
 
-    public static String getSmallTriangle() {
-        String result = "";
+    public static String getSmallTriangle(int row) {
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < i;  j++) {
-                result += "*";
-            }
-        }
-        return result;
+        return TriangleUtilities.getTriangle(row);
     }
 
-    public static String getLargeTriangle() {
-        return null;
+    public static String getLargeTriangle(int row) {
+
+        return TriangleUtilities.getTriangle(row);
     }
 
 }
